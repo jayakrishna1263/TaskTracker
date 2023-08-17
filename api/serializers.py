117775d13f84f076caec416abcd4fa92eb207cd3
jayakrishna1263 <dasarijayakrishna4321@gmail.com>
2023-08-17 12:05:51 +0530

    Serializers added

--- api/serializers.py
+++ api/serializers.py
@@ -3,19 +3,40 @@ from users.models import *
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class PeopleSerializer(serializers.ModelSerializer):
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model=CustomUser
         fields="__all__"
-        #exclude=['name']
-        #fields=['name','age']
-        # depth=1
+        #fields=['username','email','role']
         
-    # def validate(self,data):  # Here data=[id,name,age]
-    #     if data['age']<18:
-    #         raise serializers.ValidationError("age should be greater than 18")
-    #     return data
-    # def validate_age(self,data):   #Here data=age
-    #     if data<18:
-    #         raise serializers.ValidationError("age should be greater than 18")
-    #     return data
+    def validate_username(self,data):
+        special_characters="!@#$%^&*()-+=<>?/_"
+        if any(c in special_characters for c in data['username']):
+            raise serializers.ValidationError("Username should not contain special chars")
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Team
+        fields="__all__"
+        
+    def validate_team_name(self,data):
+        special_characters="!@#$%^&*()-+=<>?/_"
+        if any(c in special_characters for c in data['team_name']):
+            raise serializers.ValidationError("Team name should not contain special chars")
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Task
+        fields="__all__"
+        
+    def validate_username(self,data):
+        special_characters="!@#$%^&*()-+=<>?/_"
+        if any(c in special_characters for c in data['task_name']):
+            raise serializers.ValidationError("Task name should not contain special chars")
+class TeamMemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=TeamMember
+        fields="__all__"
+class TaskAssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=TaskAssignment
+        fields="__all__"
+        
